8b4decf91dbe0113f93fe24604112fcae559eeab
Yuva Nithesh <138885995+YuvaNithesh@users.noreply.github.com>
2025-07-05 09:48:43 +0530

    Time: 0 ms (100%), Space: 40.2 MB (90.84%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -20,3 +20,10 @@ class Solution {
         return b;
     }
 }
+//class Solution {
+//    public int fib(int n) {
+//        if (n <= 1) return n;
+//        return fib(n - 1) + fib(n - 2);
+//    }
+//}
+
