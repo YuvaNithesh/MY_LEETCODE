ab84c7da3ded1311b93ce93dbe9788f629811e9e
Yuva Nithesh <138885995+YuvaNithesh@users.noreply.github.com>
2025-07-05 11:17:07 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.26%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,29 +1,33 @@
-//class Solution {
-  //  public int fib(int n) {
-    //    if(n==0||n==1){
-      //      return n;
-        //}
-        //else{
-        //return n-1;
-        //}
-   // }
-//}
+// //class Solution {
+//   //  public int fib(int n) {
+//     //    if(n==0||n==1){
+//       //      return n;
+//         //}
+//         //else{
+//         //return n-1;
+//         //}
+//    // }
+// //}
+// class Solution {
+//     public int fib(int n) {
+//         if (n <= 1) return n;
+//         int a = 0, b = 1;
+//         for (int i = 2; i <= n; i++) {
+//             int temp = a + b;
+//             a = b;
+//             b = temp;
+//         }
+//         return b;
+//     }
+// }
 class Solution {
+    int[] memo = new int[31];
     public int fib(int n) {
         if (n <= 1) return n;
-        int a = 0, b = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = a + b;
-            a = b;
-            b = temp;
-        }
-        return b;
-    }
+        if(memo[n] > 0) return memo[n];
+        memo[n] = fib(n - 1) + fib(n - 2);
+
+        return memo[n];
+   }
 }
-//class Solution {
-//    public int fib(int n) {
-//        if (n <= 1) return n;
-//        return fib(n - 1) + fib(n - 2);
-//    }
-//}
 
