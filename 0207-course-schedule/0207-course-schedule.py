e7403b7a01c5a1b5a6f2ed0d2d4b2fcb7cdf0e63
Yuva Nithesh <138885995+YuvaNithesh@users.noreply.github.com>
2025-12-02 21:13:11 +0530

    Time: 3 ms (87.78%), Space: 18.9 MB (82.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,16 @@
 from collections import deque
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites):
-    
         adj = [[] for _ in range(numCourses)]
         in_degree = [0] * numCourses
-
         for a, b in prerequisites:
-            
             adj[b].append(a)
             in_degree[a] += 1
-
         q = deque()
         for i in range(numCourses):
             if in_degree[i] == 0:
                 q.append(i)
-
         taken = 0 
-
         while q:
             course = q.popleft()
             taken += 1  
